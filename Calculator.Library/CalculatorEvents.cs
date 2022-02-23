68088fe846658e7379755f949284286fae619e63
PoonamMahi <36147799+PoonamMahi@users.noreply.github.com>
2022-02-23 03:25:14 -0500

    Added Web API with response code

---
+++ Calculator.Library/CalculatorEvents.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Calculator.Library
+{
+    public class CalculatorEvents
+    {
+        //Addition Task
+        public int Addition(int x,int y)
+        {
+            try
+            { 
+                return x + y;  //returns added result
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        //Subtraction Task
+        public int Subtraction(int x,int y)
+        {
+            try
+            { 
+                return x - y; //returns subtracted result
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        //Multiplication Task
+        public int Multiplication(int x,int y)
+        {
+            try
+            {
+                return x * y; //returns multiplication result 
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        //Division Task
+        public int Division(int x,int y)
+        {
+            try
+            {
+                return x / y; //returns Division result 
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
